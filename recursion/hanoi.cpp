ae0f8f2a493132b2e27a7133c38861a61de6b763
Moody Rahman <moody.byte@gmail.com>
2021-02-09 17:32:22 -0500

    hanoi start

---
+++ recursion/hanoi.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+
+void hanoi(int *disks)
+{
+    std::cout << "Hello!" << std::endl;
+    
+}
+
+
+
+int main(int argc, char const *argv[])
+{
+    // hanoi
+    return 0;
+}
