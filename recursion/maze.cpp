8bbeb6eba722a000fd6f98f34ef60ea31aa0cc72
Moody Rahman <moody.byte@gmail.com>
2021-02-10 20:36:26 -0500

    sort possibilites according to the one nearest the goal

--- recursion/maze.cpp
+++ recursion/maze.cpp
@@ -5,14 +5,22 @@
 #include <sstream>
 #include <iostream>
 #include <unistd.h>
+#include <cstdlib>
+#include <algorithm>
 
 struct Maze
 {
+    struct Possibity
+    {
+        int dx, dy, dist;
+    };
+
     std::vector<std::string> maze;
+    std::vector<std::vector<int>> dist;
     int dtime = 10000;
     char wall = '#';
     char empty = ' ';
-    char filled = '^';
+    char filled = '.';
     char backtrack = 'a';
 
 
@@ -23,17 +31,29 @@ struct Maze
 
     void load(std::string filename)
     {
-        std::fstream in;
+        // std::fstream in;
+        // std::string line;
+
+        // in.open(filename);
+        // while (std::getline(in, line))
+        // {
+        //     maze.push_back(line);
+        // }
+
         std::string line;
 
-        in.open(filename);
-        while (std::getline(in, line))
+        while (std::getline(std::cin, line))
         {
             maze.push_back(line);
         }
         
     }
 
+    static bool possibilityComparator(Possibity a, Possibity b)
+    {
+        return a.dist < b.dist;
+    }
+    
     bool solveHelp(int x, int y)
     {
         std::cout << x << ", " << y << std::endl;
@@ -66,22 +86,58 @@ struct Maze
             usleep(dtime);
             maze[x][y] = filled;
 
-            if (solveHelp(x, y+1))
+            int deltas[4][2] = {
+                {0, 1},
+                {0, -1},
+                {1, 0},
+                {-1, 0}
+            };
+
+            std::vector<Possibity> possibilities;
+
+            for (auto d:deltas)
             {
-                return true;
+                possibilities.push_back(
+                    Possibity{
+                        x+d[0], 
+                        y+d[1], 
+                        dist[x+d[0]][y+d[1]]
+                    }
+                );
             }
-            if (solveHelp(x, y-1))
+
+
+            std::sort(possibilities.begin(), possibilities.end(), possibilityComparator);
+
+            for (auto m:possibilities)
             {
-                return true;
+                if (solveHelp(m.dx, m.dy))
+                {
+                    return true;
+                }
+                std::cout << m.dx << " " << m.dy << std::endl;
             }
-            if (solveHelp(x+1, y))
-            {
-                return true;
-            }
-            if (solveHelp(x-1, y))
-            {
-                return true;
-            }
+
+
+
+
+
+            // if (solveHelp(x, y+1))
+            // {
+            //     return true;
+            // }
+            // if (solveHelp(x, y-1))
+            // {
+            //     return true;
+            // }
+            // if (solveHelp(x+1, y))
+            // {
+            //     return true;
+            // }
+            // if (solveHelp(x-1, y))
+            // {
+            //     return true;
+            // }
 
             maze[x][y] = backtrack;
             print();
@@ -96,8 +152,15 @@ struct Maze
 
     }
 
+    int distance(int x1, int y1, int x2, int y2)
+    {
+        return std::abs(x2 - x1) + std::abs(y2 - y1);
+    }
+
     void solve()
     {
+        int startx, starty;
+        int endx, endy;
         for (int x = 0; x < maze.size(); x++)
         {
             for (int y = 0; y < maze[x].size(); y++)
@@ -105,10 +168,40 @@ struct Maze
                 if (maze[x][y] == 'S')
                 {
                     std::cout << "HERE" << std::endl;
-                    solveHelp(x, y);
+                    startx = x;
+                    starty = y;
+                }
+                if (maze[x][y] == 'E')
+                {
+                    endx = x;
+                    endy = y;
                 }
             }
         }
+
+        std::vector<int> row;
+
+        for (int x = 0; x < maze.size(); x++)
+        {
+            for (int y = 0; y < maze[x].size(); y++)
+            {
+                row.push_back(distance(x, y, endx, endy));
+            }
+            dist.push_back(row);
+            row.clear();
+        }
+
+
+        // for(auto x: dist)
+        // {
+        //     for (auto y: x)
+        //     {
+        //         std::cout << y << " ";
+        //     }
+        //     std::cout << "" << std::endl;
+            
+        // }
+        solveHelp(startx, starty);
     }
 
     void print()
@@ -155,6 +248,11 @@ int main(int argc, char const *argv[])
 
     m.solve();
 
+    
+    // int x = m.distance(7, 8, 10, 3);
+
+    // std::cout << x << std::endl;
+    
 
     return 0;
 }
